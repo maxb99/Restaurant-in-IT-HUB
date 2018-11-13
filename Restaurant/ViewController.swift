f937a6429050c39d977867710d9eec6a9917a925
Denis Bystruev <dbystruev@me.com>
2018-11-13 10:08:33 +0300

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Denis Bystruev on 13/11/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
